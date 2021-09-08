25d8caa042b119660f7020099aabb6d392c10874
ddatunashvili <davitidatunashvili98@gmail.com>
2021-09-09 03:43:01 +0400

    fixed

--- EX_3.py
+++ EX_3.py
@@ -1,19 +1,30 @@
 def check(string):
     crt = {'open': 1}
+    count = 0
     for i in string:
+        # 0 სწორია შეცვლილი ან დაამატე 0
         if i == "(":
+            # თუ ღიაა სწორია
             if crt['open'] == 0:
-                return 'სწორია'
+                return 'არასწორია'
+
+            # გახსნა
             else:
                 crt["open"] = 0
 
+        # 1 არასწორია ან 0 იანია და დახურე
         elif i == ")":
+            # თუ ღია არაა არასწორია
             if crt["open"] != 0:
                 return "არასწორია"
+            # დახურვა
             else:
-                crt.pop("open")
+                crt["open"] = 1
 
-    return 'სტრიქონი არ შედება “(“ და “)“ ელემენტებისგან'
+    if crt['open'] == 0:
+        return 'არასწორია'
 
+    elif crt['open'] == 1:
+        return "სწორია"
 
-check(" ")
+print(check(""))
